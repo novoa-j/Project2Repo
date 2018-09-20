5e86f9520560178b99b57a32b682ff084a5e814b
Jonathan Novoa <jon142536@gmail.com>
2018-09-20 02:12:09 -0400

    AccountDaoImpl complete

--- salutem/src/main/java/com/revature/salutem/daos/AccountDao.java
+++ salutem/src/main/java/com/revature/salutem/daos/AccountDao.java
@@ -1,8 +1,16 @@
 package com.revature.salutem.daos;
 
+import java.util.List;
+
+import com.revature.salutem.models.Account;
+
 public interface AccountDao {
 
-	public int createAccount();
-	public int deleteAccount();
-//	public int 
+	public int createAccount( String username, String key);
+	public int deleteAccountById(int id);
+	public List<Account> getAllAccounts();
+	public Account	getAccountById(int id);
+	public int updateAccountById(int id, String username, String key);
+	
+	
 }
