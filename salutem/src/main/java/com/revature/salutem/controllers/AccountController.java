521a98d07dec67bac49f85de290ea64ec535f807
11crandall <11ccrandall@gmail.com>
2018-09-22 21:53:01 -0500

    Finalized AccountController

--- salutem/src/main/java/com/revature/salutem/controllers/AccountController.java
+++ salutem/src/main/java/com/revature/salutem/controllers/AccountController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,4 +48,13 @@ public class AccountController {
 		return accServ.updateAccount(acc);
 	}
 	
+	@DeleteMapping(consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public void deleteAccount(@RequestBody Account acc) {
+		accServ.deleteAccount(acc);
+	}
+	
+	@DeleteMapping(value="/{id}",consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public void deleteAccount(@PathVariable("id") int id) {
+		accServ.deleteAccountById(id);
+	}	
 }
