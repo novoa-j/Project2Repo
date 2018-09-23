970a6a0fc5f87e150e7ac9483d30c4bd81296ed2
Jonathan Novoa <jon142536@gmail.com>
2018-09-22 20:34:03 -0400

    Get by Id controller mapping

--- salutem/src/main/java/com/revature/salutem/controllers/AccountController.java
+++ salutem/src/main/java/com/revature/salutem/controllers/AccountController.java
@@ -2,12 +2,17 @@ package com.revature.salutem.controllers;
 
 import java.util.List;
 
+import javax.security.auth.login.AccountNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.exceptions.AccountIdNotFoundException;
 import com.revature.salutem.models.Account;
 import com.revature.salutem.services.AccountDaoImpl;
 
@@ -22,4 +27,19 @@ public class AccountController {
 	public List<Account> getAllAccounts(){
 		return accServ.getAllAccounts();
 	}
+	
+	@GetMapping("/accounts/{id}")
+	@ResponseBody
+	public Account getBearByPathId(@PathVariable("id") int id)  {
+		Account a = accServ.getAccountById(id);
+		if(a == null) {
+			throw new AccountIdNotFoundException();
+		}
+		return a;
+	}
+	
+	
+	
+	
+	
 }
--- salutem/src/main/java/com/revature/salutem/repositories/AccountRepository.java
+++ salutem/src/main/java/com/revature/salutem/repositories/AccountRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.revature.salutem.models.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+	public Account findAccountByUsername(String username);
+	public Account findAccountByAccountid(int id);
+	public Account findAccountByUsernameAndKey(String username, String key);
 
 }
