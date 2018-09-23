5a6dd35307c99b942dfcb1ee4e723c65c86ce246
Jonathan Novoa <jon142536@gmail.com>
2018-09-22 22:53:00 -0400

    Change to Boot Project

---
+++ salutem/src/main/java/com/revature/salutem/controllers/AccountController.java
@@ -0,0 +1,45 @@
+package com.revature.salutem.controllers;
+
+import java.util.List;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.exceptions.AccountIdNotFoundException;
+import com.revature.salutem.models.Account;
+import com.revature.salutem.services.AccountDaoImpl;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+	@Autowired
+	AccountDaoImpl accServ;
+	
+	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Account> getAllAccounts(){
+		return accServ.getAllAccounts();
+	}
+	
+	@GetMapping(value="/{id}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public Account getBearByPathId(@PathVariable("id") int id)  {
+//		System.out.println("########################################################" + id);
+		Account a = accServ.getAccountById(id);
+		if(a == null) {
+			throw new AccountIdNotFoundException();
+		}
+		return a;
+	}
+	
+	
+	
+	
+	
+}
---
+++ salutem/src/main/java/com/revature/salutem/controllers/SubmissionController.java
@@ -0,0 +1,15 @@
+package com.revature.salutem.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.salutem.services.SubmissionDaoImpl;
+
+@RestController
+@RequestMapping("/submissions")
+public class SubmissionController {
+
+	@Autowired
+	SubmissionDaoImpl subServ;
+}
