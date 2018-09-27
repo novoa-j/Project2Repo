89dc95bfedd7c7e2138e4fed3a6f040304e21cb7
Nozuko Sutherland <nozukosutherland@gmail.com>
2018-09-26 20:38:42 -0400

    added body symptoms to body locations component, incomplete

--- angular-salutem/src/app/body-locations/body-locations.component.ts
+++ angular-salutem/src/app/body-locations/body-locations.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BodyLocation } from '../body-location';
 import { HealthResultService } from '../services/health-result.service';
+import { BodySymptom } from '../symptom';
 
 @Component({
   selector: 'app-body-locations',
@@ -18,38 +19,82 @@ export class BodyLocationsComponent implements OnInit {
   bodyLocations: BodyLocation[] = [];
   bodySubLocations: BodyLocation[] = [];
 
-
   bodyId: number;
 
   // must castHTMLElement as <HTMLInputElement> to use .value method
-  getBodyLocations(){
+  getBodyLocations(): number{
     if ((<HTMLInputElement>document.getElementById("bodyLocationSelector")).value === "choose")
       document.getElementById("bodySubLocationSelector").setAttribute("disabled", "boolean");
     else{
       document.getElementById("bodySubLocationSelector").removeAttribute("disabled");
       this.bodyId = parseInt((<HTMLInputElement>document.getElementById("bodyLocationSelector")).value);
+      //this.retBodyId();
+      localStorage.setItem("bodyId", this.bodyId + "");
+      console.log("bodyId" + this.bodyId);
       this.healthResultService.loadBodyLocation(this.bodyId)
           .subscribe((allBodyLocations) => {
               this.bodySubLocations = allBodyLocations;
               this.populateSubLocations();
             });
         }
+        return this.bodyId;
   }
 
-populateSubLocations() {
-  if ((<HTMLInputElement>document.getElementById("bodyLocationSelector")).value != "choose"){
-    let optionsList = document.getElementById("bodySubLocationSelector");
-    console.log("optionslist:  " + optionsList);
-    optionsList.innerHTML = "";
-    this.bodySubLocations.forEach(element => {
-      let tmpOption = document.createElement("option");
-      tmpOption.setAttribute("value", element.ID + "");
-      tmpOption.innerText = element.Name;
-      optionsList.appendChild(tmpOption);
-    });
+  populateSubLocations() {
+    if ((<HTMLInputElement>document.getElementById("bodyLocationSelector")).value != "choose"){
+      let optionsList = document.getElementById("bodySubLocationSelector");
+      //console.log("optionslist:  " + JSON.stringify(optionsList));
+      optionsList.innerHTML = "";
+      this.bodySubLocations.forEach(element => {
+        let tmpOption = document.createElement("option");
+        tmpOption.setAttribute("value", element.ID + "");
+        tmpOption.innerText = element.Name;
+        optionsList.appendChild(tmpOption);
+      });
+    }
   }
-}
 
+  // ----------------------------------------------------------------------------------
+  // Body Symptoms:
 
+  isClicked: boolean = false;
+
+  bodySymptoms: BodySymptom[] = [];
+
+  //selectedId: number = this.bodyLocationId.getBodyLocations();
+  id: number;
+  gender: string;
+
+  genders = ["male", "female", "boy", "girl"];
+
+  getBodySymptoms(){
+    this.changeClicked();
+    this.healthResultService.loadBodySymptoms(parseInt(localStorage.getItem("bodyId")), this.convertGender(this.gender)).subscribe((allBodySymptoms) => {this.bodySymptoms = allBodySymptoms});
+    console.log("gender entered: " + this.gender);
+    console.log("converting gender to: " + this.convertGender(this.gender));
+    console.log("the bodyId:  " + localStorage.getItem("bodyId"));
+  }
+
+  changeClicked(){
+    this.isClicked = !this.isClicked;
+  }
+
+  convertGender(gen: string): number {
+    if (gen == "male") {
+      return 0;
+    }
+    if (gen == "female") {
+      return 1;
+    }
+    if (gen == "boy") {
+      return 2;
+    }
+    if (gen == "girl") {
+      return 3;
+    }
+    else {
+      return 0;
+    }
+  } // end convert gender
 
 }
